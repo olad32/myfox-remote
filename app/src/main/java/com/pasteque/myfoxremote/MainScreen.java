1e11561c5c77193f76b016f2c4879a8a2430c7a1
olad32 <olad32@gmail.com>
2015-10-12 19:21:40 +0200

    Initial commit

---
+++ app/src/main/java/com/pasteque/myfoxremote/MainScreen.java
@@ -0,0 +1,18 @@
+package com.pasteque.myfoxremote;
+
+import android.app.IntentService;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+    public void desactiver(View view) {
+        IntentService service
+    }
+}
