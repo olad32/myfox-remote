8dba73a46db747760974cf7cc280520c8566596f
olad32 <olad32@gmail.com>
2015-10-13 11:53:21 +0200

    bouton settings + divers

--- app/src/main/java/com/pasteque/myfoxremote/MainScreen.java
+++ app/src/main/java/com/pasteque/myfoxremote/MainScreen.java
@@ -1,8 +1,11 @@
 package com.pasteque.myfoxremote;
 
-import android.app.IntentService;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -12,7 +15,29 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main_screen, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openSettings() {
+    }
+
     public void desactiver(View view) {
-        IntentService service
+        //IntentService service
     }
 }
